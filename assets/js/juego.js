08b520eb956b34c7ee6e855813606736c4955958
ronnysanga <rsanga@espol.edu.ec>
2022-01-25 18:24:21 -0500

    lista completa

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -1,11 +1,22 @@
 
-let deck = [];
+let   deck       = [];
+const tipos      = ["C","D","H","S"];
+const especiales = ["A","J","Q","K"];
 
 const crearDeck = () => {
-    for (let i = 2; i<=10;i++){
-        deck.push(i+"C");
+    for (let i = 2; i<=10;i++){    
+        for (let tipo of tipos){
+            deck.push(i+tipo);
+        }
     } 
+    for (let tipo of tipos){    
+        for (let esp of especiales){
+            deck.push(esp+tipo);
+        }
+    }
 }
+
 
 crearDeck();
 console.log(deck);
+
